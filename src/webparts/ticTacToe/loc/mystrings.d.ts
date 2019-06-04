b4c13b4f305595aa23a2ed13c9a8d51c118f00c2
Deivydas Kinderis <kalted1997@gmail.com>
2019-06-04 15:20:53 +0300

    Initial commit

---
+++ src/webparts/ticTacToe/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITicTacToeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TicTacToeWebPartStrings' {
+  const strings: ITicTacToeWebPartStrings;
+  export = strings;
+}
