2ffafc1d866bea2e98fadba723ff805a7f466515
Deivydas Kinderis <kalted1997@gmail.com>
2019-06-04 21:33:17 +0300

    Basic tic tac toe game single player

--- src/webparts/ticTacToe/TicTacToeWebPart.ts
+++ src/webparts/ticTacToe/TicTacToeWebPart.ts
@@ -1,24 +1,25 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
+} from "@microsoft/sp-property-pane";
 
-import * as strings from 'TicTacToeWebPartStrings';
-import TicTacToe from './components/TicTacToe';
-import { ITicTacToeProps } from './components/ITicTacToeProps';
+import * as strings from "TicTacToeWebPartStrings";
+import TicTacToe from "./components/TicTacToe";
+import { ITicTacToeProps } from "./components/props/ITicTacToeProps";
 
 export interface ITicTacToeWebPartProps {
   description: string;
 }
 
-export default class TicTacToeWebPart extends BaseClientSideWebPart<ITicTacToeWebPartProps> {
-
+export default class TicTacToeWebPart extends BaseClientSideWebPart<
+  ITicTacToeWebPartProps
+> {
   public render(): void {
-    const element: React.ReactElement<ITicTacToeProps > = React.createElement(
+    const element: React.ReactElement<ITicTacToeProps> = React.createElement(
       TicTacToe,
       {
         description: this.properties.description
@@ -33,7 +34,7 @@ export default class TicTacToeWebPart extends BaseClientSideWebPart<ITicTacToeWe
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -47,7 +48,7 @@ export default class TicTacToeWebPart extends BaseClientSideWebPart<ITicTacToeWe
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
+                PropertyPaneTextField("description", {
                   label: strings.DescriptionFieldLabel
                 })
               ]
---
+++ src/webparts/ticTacToe/components/props/ISquareProps.ts
@@ -0,0 +1,4 @@
+export interface ISquareProps {
+  value: string;
+  onClick: () => void;
+}
